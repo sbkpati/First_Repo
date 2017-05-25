4263ce67b72f5c2cbe321d5d06917237a685a63e
sbkpati <sbkpati@gmail.com>
2017-05-24 23:41:31 -0500

    First commit
    Change-Id: I45d4c43ba7be72374f53164152d4080779e6d6e8
    Signed-off-by: sbkpati <sbkpati@gmail.com>

---
+++ src/git/example/JavaClass.java
@@ -0,0 +1,13 @@
+package git.example;
+
+public class JavaClass {
+	static int a=5;
+	static int b=12;
+	public static void main(String[] args) {
+		System.out.println("Hello World.");
+		System.out.println("I started java programming.");
+		System.out.println("Sum of a+b= "+(a+b));
+
+	}
+
+}
