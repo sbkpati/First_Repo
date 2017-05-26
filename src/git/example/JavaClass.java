4c1fe2513260ab3edb64fa2773b92765bfe3a7d5
sbkpati <sbkpati@gmail.com>
2017-05-25 20:26:55 -0500

    third commit
    Change-Id: I4d9725ec791fb990d4e76bc48ce4c8ea124e8cd6
    Signed-off-by: sbkpati <sbkpati@gmail.com>

--- src/git/example/JavaClass.java
+++ src/git/example/JavaClass.java
@@ -2,7 +2,7 @@ package git.example;
 
 public class JavaClass {
 	static int a=5;
-	static int b=10;
+	static int b=13;
 	public static void main(String[] args) {
 		System.out.println("Hello World.");
 		System.out.println("I started java programming.");
